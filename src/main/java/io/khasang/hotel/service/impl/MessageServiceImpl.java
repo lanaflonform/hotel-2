f40273d155ddc5045383f5d15356b92c0d4f6bd1
Azon <azon.sk@gmail.com>
2017-11-12 20:59:10 +0300

    add hibernate core

--- src/main/java/io/khasang/hotel/service/impl/MessageServiceImpl.java
+++ src/main/java/io/khasang/hotel/service/impl/MessageServiceImpl.java
@@ -3,10 +3,17 @@ package io.khasang.hotel.service.impl;
 import io.khasang.hotel.service.MessageService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service("messageService")
 public class MessageServiceImpl implements MessageService {
     @Override
     public String getMessageInfo(String info) {
         return info;
+    }
+
+    @PostConstruct
+    public void init(){
+
     }
 }
