994910fae3ff1e180e061154797417ad741ca3ad
Denis Sudakov <des.sudakov@gmail.com>
2017-11-26 21:00:05 +0500

    add column name for first name and last name

--- src/main/java/io/khasang/hotel/entity/User.java
+++ src/main/java/io/khasang/hotel/entity/User.java
@@ -11,7 +11,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     @Column(unique = true)
