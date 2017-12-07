8c8c0466d421282a1c9d21c0f86380a8de08a022
Denis Sudakov <des.sudakov@gmail.com>
2017-12-07 06:57:25 +0500

    switch default constructor on @NoArgsConstructor

--- src/main/java/io/khasang/hotel/dto/RoleDTO.java
+++ src/main/java/io/khasang/hotel/dto/RoleDTO.java
@@ -1,15 +1,14 @@
 package io.khasang.hotel.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class RoleDTO {
     private Long id;
     private String name;
     private String description;
-
-    public RoleDTO() {
-    }
 
     public RoleDTO(Long id, String name, String description) {
         this.id = id;
