33063f2e3798a0fef23b2297fc5baa401225fca2
Denis Sudakov <des.sudakov@gmail.com>
2017-12-07 12:29:01 +0500

    add missing id in constructor

--- src/main/java/io/khasang/hotel/entity/Role.java
+++ src/main/java/io/khasang/hotel/entity/Role.java
@@ -14,6 +14,7 @@ public class Role {
     }
 
     public Role(Long id, String name, String description) {
+        this.id = id;
         this.name = name;
         this.description = description;
     }
